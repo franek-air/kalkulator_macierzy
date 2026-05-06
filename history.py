cd03701c3ace90507f6ec9debbecdbd5c76936af
franek-air <franek.grzesik@gmail.com>
2026-05-06 18:02:17 +0200

    zarys funkcji historii

--- history.py
+++ history.py
@@ -1,1 +1,14 @@
+# Lista przechowująca max 10 ostatnich wyników
+historia = []
 
+def dodaj_do_historii(operacja, wynik):
+    # operacja to string (np. "Dodawanie"), wynik to macierz
+    # Pamiętaj o limicie 10 elementów!
+    pass
+
+def wyswietl_historie():
+    pass
+
+def wyczysc_historie():
+    global historia
+    historia = []
